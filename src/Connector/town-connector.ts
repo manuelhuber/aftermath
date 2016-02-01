d947172672fd8e2a4d9876b76c7e8be8811f5af7
nelo112 <nelo112@web.de>
2016-02-01 20:29:24 +0100

    Added town service functions

--- src/Connector/town-connector.ts
+++ src/Connector/town-connector.ts
@@ -1,26 +1,156 @@
 import {Injectable} from 'angular2/core';
 
+/**
+ * Just a mock implementation.
+ * Will later do all of the http REST calls for anything Town related
+ */
 @Injectable()
 export class TownConnector {
 
     town : TownModel;
+    tiers : TierModel[];
+
+    constructor () {
+        this.town = require('./mock-data/town.json');
+        this.tiers = require('./mock-data/tiers.json');
+    }
 
     getTown () : Promise<TownModel> {
         return new Promise((resolve : any) => {
-            resolve(require('./mock-data/town.json'));
+            resolve(this.town);
         });
     }
 
-    getTier (upgradeable : string, tier : number) : Promise<TierModel> {
-        let result : TierModel = {
-            tier: 1,
-            upgradeGoldCost: 1,
-            upgradeShardCost: 1,
-            upgradeSizeRequirement: 1,
-            description: 'test',
-        };
+    upgradeGnarfHQ (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    upgradeTaverne (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    upgradeFarm (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    upgradeTownGuard (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    upgradeHospital (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    upgradeTradingPost (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    upgradeShrine (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    upgradeLibrary (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    upgradeStables (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    upgradeBarracks (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    upgradeAlchemist (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    upgradeBlacksmith (characterId : number) : Promise<any> {
+        console.log('removed gold from character, update Town');
+        return new Promise((resolve : any) => { resolve(); });
+    }
+
+    getGnarfHQTiers () : Promise<TierModel[]> {
         return new Promise((resolve : any) => {
-            resolve(result);
+            resolve(this.tiers);
         });
     }
+
+    getTaverneTiers () : Promise<TierModel[]> {
+        return new Promise((resolve : any) => {
+            resolve(this.tiers);
+        });
+    }
+
+    getFarmTiers () : Promise<TierModel[]> {
+        return new Promise((resolve : any) => {
+            resolve(this.tiers);
+        });
+    }
+
+    getTownGuardTiers () : Promise<TierModel[]> {
+        return new Promise((resolve : any) => {
+            resolve(this.tiers);
+        });
+    }
+
+    getHospitalTiers () : Promise<TierModel[]> {
+        return new Promise((resolve : any) => {
+            resolve(this.tiers);
+        });
+    }
+
+    getTradingPostTiers () : Promise<TierModel[]> {
+        return new Promise((resolve : any) => {
+            resolve(this.tiers);
+        });
+    }
+
+    getShrineTiers () : Promise<TierModel[]> {
+        return new Promise((resolve : any) => {
+            resolve(this.tiers);
+        });
+    }
+
+    getLibraryTiers () : Promise<TierModel[]> {
+        return new Promise((resolve : any) => {
+            resolve(this.tiers);
+        });
+    }
+
+    getStablesTiers () : Promise<TierModel[]> {
+        return new Promise((resolve : any) => {
+            resolve(this.tiers);
+        });
+    }
+
+    getBarracksTiers () : Promise<TierModel[]> {
+        return new Promise((resolve : any) => {
+            resolve(this.tiers);
+        });
+    }
+
+    getAlchemistTiers () : Promise<TierModel[]> {
+        return new Promise((resolve : any) => {
+            resolve(this.tiers);
+        });
+    }
+
+    getBlacksmithTiers () : Promise<TierModel[]> {
+        return new Promise((resolve : any) => {
+            resolve(this.tiers);
+        });
+    }
+
 }
