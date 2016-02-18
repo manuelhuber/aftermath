18935432efcba02be13f91d783fb9cb4cb118697
Manuel Huber <nelo112@web.de>
2016-02-18 18:47:52 +0100

    Replaced hardcoded images with image url from character model; minor styling stuff

--- src/app/town/character-list/character-details/character-details.ts
+++ src/app/town/character-list/character-details/character-details.ts
@@ -1,9 +1,10 @@
 // Services
-import { Component, Input, EventEmitter, Output } from 'angular2/core';
+import { Component, Input, EventEmitter, Output, Inject, OnChanges, SimpleChange } from 'angular2/core';
 import { NgIf } from 'angular2/common';
 
 // Style
 import './character-details.less';
+import {CharacterService} from '../../../../service/character-service';
 
 @Component({
     selector: 'character-details',
@@ -14,13 +15,28 @@ export class CharacterDetails {
     @Input() active : boolean = true;
     @Input() selectedCharacterId : number;
     @Input() characterIds : number[];
+    character : CharacterModel;
 
     // Needed for two way binding
     @Output() activeChange : EventEmitter<boolean> = new EventEmitter();
 
+    constructor (@Inject(CharacterService) private characterService : CharacterService) {
+    }
+
     close (event : Event) : void {
         if (event.srcElement.classList.contains('character-details-background')) {
+            this.active = false;
             this.activeChange.emit(false);
+        }
+    }
+
+    ngOnChanges (changes : {[propName: string]: SimpleChange}) : void {
+        if (!!changes['selectedCharacterId']) {
+            console.log(this.selectedCharacterId);
+            this.characterService.getCharacterByID(this.selectedCharacterId).then((character : CharacterModel) => {
+                this.character = character;
+                console.log(character);
+            });
         }
     }
 
