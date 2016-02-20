b7d01b08708c4a186bca823afd64eb898d100356
Manuel Huber <nelo112@web.de>
2016-02-20 12:17:19 +0100

    Refactored GNARF to Aftermath

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -22,7 +22,7 @@ var HOST = process.env.HOST || 'localhost';
 var PORT = process.env.PORT || 8080;
 
 var metadata = {
-  title: 'GNARF',
+  title: 'Aftermath',
   baseUrl: '/',
   host: HOST,
   port: PORT,
