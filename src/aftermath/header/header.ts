9d00071fa6e56de88352c23b843afa76c8d9b9b5
Manuel Huber <nelo112@web.de>
2016-02-20 22:52:04 +0100

    Added random jitter to header text

--- src/aftermath/header/header.ts
+++ src/aftermath/header/header.ts
@@ -1,7 +1,7 @@
 /*
  * Angular 2 decorators and services
  */
-import { Component } from 'angular2/core';
+import { Component, AfterViewInit } from 'angular2/core';
 
 import './header.less';
 
@@ -13,6 +13,18 @@ import './header.less';
     selector: 'header',
     template: require('./header.html')
 })
-export class Header {
+export class Header implements AfterViewInit {
+
+    ngAfterViewInit () : any {
+        let text : HTMLElement = document.getElementById('header-text');
+        text.innerHTML = text.innerHTML.replace(/./g, "<div class=\"header-text-letter\">$&</div>");
+
+        let letters : NodeListOf<Element> = document.getElementsByClassName('header-text-letter');
+        for (let i : number = 0; i < letters.length; i++) {
+            let html : HTMLElement = <HTMLElement>letters.item(i);
+            html.style.transform = 'rotate(' + (Math.floor(Math.random() * 20) - 10) + 'deg) ' +
+                'translateY(' + (Math.floor(Math.random() * 10) - 5) + '%)';
+        }
+    }
 
 }
