9c444be8a9e7583d15f92e28813e03d97f2f092e
Manuel Huber <nelo112@web.de>
2016-02-21 00:01:36 +0100

    Moved Links to header

--- src/aftermath/header/header.ts
+++ src/aftermath/header/header.ts
@@ -2,8 +2,11 @@
  * Angular 2 decorators and services
  */
 import { Component, AfterViewInit } from 'angular2/core';
+import { RouteConfig, Router, ROUTER_DIRECTIVES } from 'angular2/router';
 
 import './header.less';
+import {Login} from '../login/login';
+import {TownOverview} from '../town/town-overview';
 
 /*
  * App Component
@@ -11,6 +14,7 @@ import './header.less';
  */
 @Component({
     selector: 'header',
+    directives: [ROUTER_DIRECTIVES],
     template: require('./header.html')
 })
 export class Header implements AfterViewInit {
@@ -22,7 +26,7 @@ export class Header implements AfterViewInit {
         let letters : NodeListOf<Element> = document.getElementsByClassName('header-text-letter');
         for (let i : number = 0; i < letters.length; i++) {
             let html : HTMLElement = <HTMLElement>letters.item(i);
-            html.style.transform = 'rotate(' + (Math.floor(Math.random() * 20) - 10) + 'deg) ' +
+            html.style.transform = 'rotate(' + (Math.floor(Math.random() * 10) - 5) + 'deg) ' +
                 'translateY(' + (Math.floor(Math.random() * 10) - 5) + '%)';
         }
     }
