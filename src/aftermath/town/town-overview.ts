15f8f8a970ef1334fef1e0dc5313a467713fc6b1
Manuel Huber <nelo112@web.de>
2016-02-21 10:40:02 +0100

    Added horizontal-menu stub

--- src/aftermath/town/town-overview.ts
+++ src/aftermath/town/town-overview.ts
@@ -2,12 +2,14 @@
 import { Component, Inject } from 'angular2/core';
 import { TownService } from '../../service/town-service';
 
+import { HorizontalMenu } from '../../components/horizontal-menu/horizontal-menu';
 
 // Style
 import './town-overview.less';
 
 @Component({
     selector: 'town-overview',
+    directives: [HorizontalMenu],
     providers: [TownService],
     template: require('./town-overview.html')
 })
---
+++ src/components/horizontal-menu/horizontal-menu.ts
@@ -0,0 +1,16 @@
+// Serivces
+import { Component } from 'angular2/core';
+
+// Subcomponents
+import './../../../assets/inline-icons.svg';
+
+// Style
+import './horizontal-menu.less';
+
+@Component({
+    selector: 'horizontal-menu',
+    template: require('./horizontal-menu.html')
+})
+export class HorizontalMenu {
+}
+
