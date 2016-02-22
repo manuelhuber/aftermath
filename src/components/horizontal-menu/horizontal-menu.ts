71842c52fd3f0fee38732e5b2d113670b9ef6783
Manuel Huber <nelo112@web.de>
2016-02-22 23:38:57 +0100

    Horizontal menu no sets its own width and is collapsable

--- src/components/horizontal-menu/horizontal-menu.ts
+++ src/components/horizontal-menu/horizontal-menu.ts
@@ -1,12 +1,15 @@
 // Serivces
 import { Component, Inject, NgZone } from 'angular2/core';
 
+import { InlineIcon } from '../inline-icon/inline-icon';
+
 // Style
 import './horizontal-menu.less';
 
 @Component({
     selector: 'horizontal-menu',
-    template: require('./horizontal-menu.html')
+    template: require('./horizontal-menu.html'),
+    directives: [InlineIcon]
 })
 /**
  * A horizontal menu, to be filled with horizontal menu entries
@@ -15,21 +18,20 @@ import './horizontal-menu.less';
  */
 export class HorizontalMenu {
     stickToTop : boolean;
+    menuClosed : boolean;
 
     constructor (@Inject(NgZone) zone : NgZone) {
         window.onscroll = ( (event : Event) => {
             zone.run(() => {
                 let anchor : HTMLElement = document.getElementById('horizontal-menu-sticky-anchor');
-                let menu : HTMLElement = document.getElementById('horizontal-menu');
-                // Since the width is given to the menu by the parent (and we would lose it once the menu gets the
-                // "horizontal-menu-sticky" class we need to set the width explicitly.
-                menu.style.width = menu.scrollWidth + 'px';
+                let menu : HTMLElement = document.getElementById('horizontal-menu-sticky');
                 this.stickToTop = window.pageYOffset > anchor.offsetTop - 10;
-                // Remove the hardcoded width to no longer override CSS
-                if (!this.stickToTop){
-                    menu.style.width = '';
-                }
             });
         });
+    }
+
+    toggleMenu(value ?: boolean) : void {
+        console.log('geb');
+        this.menuClosed = value === undefined ? !this.menuClosed : value;
     }
 }
