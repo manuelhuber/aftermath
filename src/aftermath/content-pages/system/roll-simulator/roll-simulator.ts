5a54cedd83ab3f77cd47cb4980a24c94203b11eb
Manuel Huber <nelo112@web.de>
2016-02-26 21:14:56 +0100

    Added rolling & success calculation to roll simulator

--- src/aftermath/content-pages/system/roll-simulator/roll-simulator.ts
+++ src/aftermath/content-pages/system/roll-simulator/roll-simulator.ts
@@ -12,15 +12,40 @@ import './roll-simulator.less';
 })
 export class RollSimulator {
 
-    baseNumber : number = 64;
-    modifierNumber : number = 040;
+    baseNumber : number = 36;
+    modifierNumber : number = 15;
+    rollNumber : number;
+
+    checkFailed : boolean;
 
     constructor () {
         console.log();
     }
 
+    get targetNumber () : number {
+        return this.baseNumber + this.modifierNumber;
+    }
+
+    get result () : number {
+        if (this.rollNumber > this.targetNumber) {
+            this.checkFailed = true;
+            return this.getDegreesOfResult(this.rollNumber, this.targetNumber);
+        } else {
+            this.checkFailed = false;
+            return this.getDegreesOfResult(this.targetNumber, this.rollNumber);
+        }
+    }
+
     getModifierHeight () : number {
         return Math.abs(this.modifierNumber) * 0.7;
+    }
+
+    rollD100 () : void {
+        this.rollNumber = Math.floor(Math.random() * 100) + 1;
+    }
+
+    getDegreesOfResult (biggerNumber : number, smallerNumber : number) {
+        return 1 + (Math.floor(biggerNumber / 10) - (Math.floor(smallerNumber / 10)));
     }
 
 }
