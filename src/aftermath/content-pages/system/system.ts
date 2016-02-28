d5cba2004f7f7b0b5913e864e8b8ffcc20dfee3b
Manuel Huber <nelo112@web.de>
2016-02-28 22:55:35 +0100

    Changed the way the horizontal menu click events are handled

--- src/aftermath/content-pages/system/system.ts
+++ src/aftermath/content-pages/system/system.ts
@@ -4,7 +4,6 @@ import { Component, Inject } from 'angular2/core';
 import { HorizontalMenu } from '../../../components/horizontal-menu/horizontal-menu';
 import { HorizontalMenuEntry } from '../../../components/horizontal-menu/horizontal-menu-entry/horizontal-menu-entry';
 import { RollSimulator } from './roll-simulator/roll-simulator';
-import { scrollSmooth } from '../../../util/scroll-util';
 
 // Style
 import './system.less';
@@ -16,9 +15,6 @@ import './system.less';
 })
 export class System {
 
-    scrollTo (event : Event, elementID : string) : void {
-        // Prevent event bubbling, so the click event of parent entry is not activated
-        event.stopPropagation();
-        scrollSmooth(document.getElementById(elementID), 250);
-    }
+    constructor () {}
+
 }
--- src/components/horizontal-menu/horizontal-menu-entry/horizontal-menu-entry.ts
+++ src/components/horizontal-menu/horizontal-menu-entry/horizontal-menu-entry.ts
@@ -1,24 +1,82 @@
 // Serivces
-import { Component, Input, ElementRef } from 'angular2/core';
+import { Component, Input, ElementRef, ContentChildren, QueryList, AfterContentInit } from 'angular2/core';
 import { NgIf } from 'angular2/common';
-import {HorizontalMenu} from '../horizontal-menu';
+import { HorizontalMenu } from '../horizontal-menu';
+import { scrollSmooth } from '../../../util/scroll-util';
+import { InlineIcon } from '../../inline-icon/inline-icon';
 
 // Style
 import './horizontal-menu-entry.less';
 
 @Component({
     selector: 'horizontal-menu-entry',
-    template: require('./horizontal-menu-entry.html')
+    template: require('./horizontal-menu-entry.html'),
+    directives: [InlineIcon]
 })
-export class HorizontalMenuEntry {
+export class HorizontalMenuEntry implements AfterContentInit {
 
+    // The actual text of the menu entry
     @Input() text : string;
+    // The HTML ID of the element to scrollTo
+    @Input() scrollTo : string;
+    // Function that will be called on click
+    @Input('onClick') inputFunction : () => void;
+
+    // The content of this component. It contains at least 1 HorizontalMenuEntry (itself)
+    //noinspection TypeScriptValidateTypes
+    @ContentChildren(HorizontalMenuEntry) children : QueryList<HorizontalMenuEntry>;
+
     showSub : boolean;
+    hasSub : boolean;
 
     constructor (private menu : HorizontalMenu) {}
 
-    toggleSub () : void {
-        this.showSub = !this.showSub;
+    /**
+     * Check for subs, once the content is initialized
+     */
+    ngAfterContentInit () : any {
+        // If there are more than 1 menu entry in this component = there is a sub
+        this.hasSub = this.children.length > 1;
+    }
+
+    /**
+     * Toggle the Sub menu
+     * if scrollTo Element is set, scroll to it
+     * if other function is given, call it
+     * @param event
+     */
+    onClick (event : Event) : void {
+
+        // Open the subs when clicking on the menu entry
+        this.toggleSub(true);
+
+        if (this.scrollTo) {
+            scrollSmooth(document.getElementById(this.scrollTo));
+        }
+
+        if (this.inputFunction) {
+            this.inputFunction();
+        }
+
+        console.log(this.children);
+
+    }
+
+    /**
+     * Toggles the sub menu entries with the given value (if given)
+     * Otherwise it just toggles to the other state.
+     * If there are no entries, nothing happens
+     */
+    toggleSub (toggle ? : boolean) : void {
+        this.showSub = toggle !== undefined ? toggle : !this.showSub;
+    }
+
+    /**
+     * Toggles the subs and stops the event from bubbling up
+     */
+    closeClick(event : Event) : void {
+        this.toggleSub();
+        event.stopPropagation();
     }
 
 }
