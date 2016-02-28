6753766b62f474b7f0a9e0e20c9c4baf068c5705
Manuel Huber <nelo112@web.de>
2016-02-28 12:46:11 +0100

    Code Style, Refactoring

--- src/aftermath/header/header.ts
+++ src/aftermath/header/header.ts
@@ -2,7 +2,7 @@
  * Angular 2 decorators and services
  */
 import { Component, AfterViewInit } from 'angular2/core';
-import { RouteConfig, Router, ROUTER_DIRECTIVES } from 'angular2/router';
+import { ROUTER_DIRECTIVES } from 'angular2/router';
 
 import './header.less';
 import {Login} from '../login/login';
@@ -18,13 +18,24 @@ import {Login} from '../login/login';
 })
 export class Header implements AfterViewInit {
 
-    ngAfterViewInit () : any {
-        let text : HTMLElement = document.getElementById('header-text');
-        text.innerHTML = text.innerHTML.replace(/./g, '<div class=\"header-text-letter\">$&</div>');
+    ngAfterViewInit () : void {
+        this.randomizeHeadline();
 
+    }
+
+    randomizeHeadline () : void {
+        //Get the Header Text
+        let text : HTMLElement = document.getElementById('header-text');
+
+        // Make every letter a seperate span with class header-text-letter
+        text.innerHTML = text.innerHTML.replace(/./g, '<span class=\"header-text-letter\">$&</span>');
+
+        // Get all the letter Elements
         let letters : NodeListOf<Element> = document.getElementsByClassName('header-text-letter');
         for (let i : number = 0; i < letters.length; i++) {
             let html : HTMLElement = <HTMLElement>letters.item(i);
+            // Random rotation between -5 and 5 degrees
+            // Random vertical movement from -5% to 5%
             html.style.transform = 'rotate(' + (Math.floor(Math.random() * 10) - 5) + 'deg) ' +
                 'translateY(' + (Math.floor(Math.random() * 10) - 5) + '%)';
         }
