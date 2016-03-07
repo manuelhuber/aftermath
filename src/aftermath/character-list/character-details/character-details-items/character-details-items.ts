a7cf4c81119073b9739551ff573a5444be217923
Manuel Huber <nelo112@web.de>
2016-03-07 21:20:36 +0100

    Changed Data Binding for CharacterDetailsItems since Angular can't detect changes within objects

--- src/aftermath/character-list/character-details/character-details-items/character-details-items.ts
+++ src/aftermath/character-list/character-details/character-details-items/character-details-items.ts
@@ -1,5 +1,5 @@
 // Services
-import { Component, Inject, Input, OnChanges } from 'angular2/core';
+import { Component, Inject, Input } from 'angular2/core';
 import { NgFor, NgIf } from 'angular2/common';
 import { CharacterService } from '../../../../service/character-service';
 
@@ -17,10 +17,9 @@ import { CharacterDetailsModel } from '../../../../model/character-details';
     directives: [SortableBoxes, SortableBox, NgFor, NgIf],
     template: require('./character-details-items.html')
 })
-export class CharacterDetailsItems implements OnChanges {
+export class CharacterDetailsItems {
 
-    @Input() details : CharacterDetailsModel;
-    items : ItemModel[];
+    @Input() items : ItemModel[];
 
     listener : EventListener;
 
@@ -38,19 +37,6 @@ export class CharacterDetailsItems implements OnChanges {
             }, 0);
         };
 
-    }
-
-    ngOnChanges (changes : {}) : any {
-
-        // This is needed so that on character change all items are removed and then readded from scratch
-
-        this.items = [];
-
-        setTimeout(() => {
-            //this.characterService.getItemsForCharacter(this.character).subscribe((items : ItemModel[]) => {
-            //    this.items = items;
-            //});
-        }, 0);
     }
 
     clickCard (event : Event) : void {
