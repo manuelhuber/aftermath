fb747630ab5478a0b67be721d9c40eb107804ef4
Manuel Huber <nelo112@web.de>
2016-03-17 08:41:21 +0100

    Added style to character-details-personality

--- src/aftermath/character-list/character-details/character-details-personality/character-details-personality.ts
+++ src/aftermath/character-list/character-details/character-details-personality/character-details-personality.ts
@@ -4,6 +4,9 @@ import { Component, Input } from 'angular2/core';
 // Sub components
 import { NgFor, NgIf } from 'angular2/common';
 
+// Style
+import './character-details-personality.less';
+
 @Component({
     selector: 'character-details-personality',
     directives: [NgFor, NgIf],
